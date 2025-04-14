e94c55e61b1d40ad4b7f639d8518e0cf069f1a50
GuilhermeGutz <guilhermegutz6@gmail.com>
2025-04-14 17:45:35 -0300

    fix banner report bug page

--- src/components/MobileMenuLinks.tsx
+++ src/components/MobileMenuLinks.tsx
@@ -144,7 +144,7 @@ export default function MobileMenuLinks({
       active: true,
       image: ArrowDropDownBlack,
       isDarkBg: true,
-    },    
+    },
   ];
 
   const handleProfileClick = () => {
