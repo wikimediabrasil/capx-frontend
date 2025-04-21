e4f18f6d190856e8ac27f128ccefb1ef60a44fb5
Leticia Nascimento <leticianascimento.dev@gmail.com>
2025-04-21 20:44:04 -0300

    Add messages backend

---
+++ src/services/messageService.ts
@@ -0,0 +1,52 @@
+import { Message } from "@/types/message";
+import axios from "axios";
+
+export interface MessageServiceParams {
+  message: Partial<Message>;
+  token: string;
+}
+
+export class MessageService {
+  static async sendMessage({
+    message,
+    token
+  }: MessageServiceParams): Promise<any> {
+    try {
+      const response = await axios.post(
+        "/api/messages",
+        {
+          receiver: message.receiver,
+          subject: message.subject,
+          message: message.message,
+          method: message.method,
+        },
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("response", response);
+      return response.data;
+    } catch (error) {
+      console.error("Failed to send message:", error);
+      throw error;
+    }
+  }
+
+  static async getMessages(token?: string): Promise<Message[]> {
+    try {
+      const response = await axios.get("/api/messages", {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Failed to get messages:", error);
+      throw error;
+    }
+  }
+}
