9fca3860015af0a13493bf8f261b9ea2e919d3df
Victor Góis <victorgois18@gmail.com>
2025-04-25 11:23:29 -0300

    Delete org profile events subpage

--- src/app/(auth)/organization_profile/[id]/edit/events/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function EditEventsPage() {
-  return (
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Edit Events</h1>
-      </div>
-  );
-}
