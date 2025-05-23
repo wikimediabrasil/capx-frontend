d648d7d9a2c0a7501f420778848d07dedd0ae76a
Leticia Nascimento <leticianascimento.dev@gmail.com>
2025-05-23 09:55:29 -0300

    Add tests to getProfileImage

--- src/lib/utils/getProfileImage.ts
+++ src/lib/utils/getProfileImage.ts
@@ -5,17 +5,20 @@ export const getProfileImage = (
   profileImage: string | null | undefined,
   avatarId: number | null | undefined,
   avatars?: Avatar[]
-) => {
-  if (profileImage) {
+): string => {
+  // If there is a profile image, use it
+  if (profileImage && profileImage.trim()) {
     return profileImage;
   }
 
-  if (avatarId && avatars) {
+  // If there is an avatar and a list of avatars, search for the avatar
+  if (avatarId && avatars?.length) {
     const avatar = avatars.find((a) => a.id === avatarId);
     if (avatar?.avatar_url) {
       return avatar.avatar_url;
     }
   }
 
+  // Fallback to the default icon
   return NoAvatarIcon;
 };
