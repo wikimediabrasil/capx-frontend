2c00283e21d691a84b3c219cdcb31a619a9bbecf
Victor Góis Pacheco <victorgois18@gmail.com>
2025-06-10 10:39:12 -0300

    Fix build

--- src/app/(auth)/organization_profile/components/OrganizationProfileDesktopView.tsx
+++ src/app/(auth)/organization_profile/components/OrganizationProfileDesktopView.tsx
@@ -31,11 +31,13 @@ import ProjectsList from "./ProjectsList";
 export default function OrganizationProfileDesktopView({
   pageContent,
   darkMode,
+  isMobile,
   organization,
   organizationId,
   token,
   isOrgManager,
   getCapacityName,
+  allCapacityIds,
 }) {
   const router = useRouter();
   const {
