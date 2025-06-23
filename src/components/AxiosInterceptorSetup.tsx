169d128820cb17ef6485a5a4735ecfd1220ba228
Victor Gois <victorgois18@gmail.com>
2025-06-23 14:31:27 -0300

    Add axios interceptor test

--- src/components/AxiosInterceptorSetup.tsx
+++ src/components/AxiosInterceptorSetup.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import setupAxiosInterceptor from "@/lib/axios-interceptor";
 
 export default function AxiosInterceptorSetup() {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
+    setIsClient(true);
+    
     // Set the global axios interceptor only on the client side
     setupAxiosInterceptor();
   }, []);
