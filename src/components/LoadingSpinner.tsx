c5c82bd78730d89c21547f5fc04a9411a9dfd653
Victor Gois <victorgois18@gmail.com>
2025-07-14 10:28:35 -0300

    Update loading spinner with aria label

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,9 +1,14 @@
+import { useApp } from '@/contexts/AppContext';
+
 export default function LoadingSpinner() {
+  const { pageContent } = useApp();
   return (
     <div
       className={
         'animate-spin ease-linear h-6 w-6 rounded-full mx-auto border-4 border-l-white border-r-white border-b-white border-t-capx-primary-green'
       }
+      role="status"
+      aria-label={pageContent['aria-label-loading-spinner'] || 'Loading...'}
     />
   );
 }
