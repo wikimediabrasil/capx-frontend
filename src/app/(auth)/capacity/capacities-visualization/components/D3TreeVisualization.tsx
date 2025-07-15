44bb5d9136bcd8d3fd04e0583c4b4c07070b81ff
Victor Gois <victorgois18@gmail.com>
2025-07-14 23:27:36 -0300

    Change text

--- src/app/(auth)/capacity/capacities-visualization/components/D3TreeVisualization.tsx
+++ src/app/(auth)/capacity/capacities-visualization/components/D3TreeVisualization.tsx
@@ -768,7 +768,7 @@ export default function D3TreeVisualization({
                 d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
               />
             </svg>
-            Mostrar Todas as Capacidades
+            Retornar a visualização inicial
           </button>
         </div>
       </div>
