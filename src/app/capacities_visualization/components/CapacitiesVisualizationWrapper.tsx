55139f579305fb91ed9905d7ba1c9fdd7a05d9a8
Victor Gois <victorgois18@gmail.com>
2025-07-19 11:35:05 -0300

    Remove unnecessary appprovider in capacities visualization wrapper

--- src/app/capacities_visualization/components/CapacitiesVisualizationWrapper.tsx
+++ src/app/capacities_visualization/components/CapacitiesVisualizationWrapper.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import CapacitiesTreeVisualization from './CapacitiesTreeVisualization';
-import { AppProvider } from '@/contexts/AppContext';
-import { ThemeProvider } from '@/contexts/ThemeContext';
+
 
 export default function CapacitiesVisualizationWrapper() {
   return (
-      <AppProvider>
         <CapacitiesTreeVisualization />
-      </AppProvider>
   ); 
 }
