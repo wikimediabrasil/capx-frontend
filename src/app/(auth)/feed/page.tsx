decf3d15f39d64c9ad549fe3b0be9788399a04d1
Guilherme Gutz <92957528+GuilhermeGutz@users.noreply.github.com>
2025-09-11 17:00:17 -0300

    fix mobile screens appears (#377)

--- src/app/(auth)/feed/page.tsx
+++ src/app/(auth)/feed/page.tsx
@@ -232,9 +232,9 @@ export default function FeedPage() {
   };
 
   return (
-    <div className="w-full flex flex-col items-center pt-24 md:pt-8">
-      <div className="container mx-auto px-4">
-        <div className="md:max-w-[1200px] w-full max-w-sm mx-auto space-y-6">
+    <div className="w-full flex flex-col items-center pt-24 md:pt-8 overflow-x-hidden">
+      <div className="w-full max-w-[1200px] mx-auto px-4">
+        <div className="w-full space-y-6">
           {/* SearchBar and Filters Button */}
           <SearchBar
             showCapacitiesSearch={true}
