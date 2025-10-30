1000b6af0922cb6230b1b7ff59c3652983460a5c
Victor Góis Pacheco <victorgois18@gmail.com>
2025-10-30 14:39:24 -0300

    Add dynamic translation to capacities visualization (#448)

--- src/app/capacities_visualization/components/CapacitiesVisualizationWrapper.tsx
+++ src/app/capacities_visualization/components/CapacitiesVisualizationWrapper.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-import { AppProvider } from '@/contexts/AppContext';
+import { useApp } from '@/contexts/AppContext';
+import { CapacityCacheProvider } from '@/contexts/CapacityCacheContext';
+import { LanguageChangeHandler } from '@/components/LanguageChangeHandler';
 import CapacitiesTreeVisualization from './CapacitiesTreeVisualization';
+
+function CapacitiesVisualizationContent() {
+  const { language } = useApp();
+
+  // Use language as key to force remount when language changes
+  return <CapacitiesTreeVisualization key={language} />;
+}
 
 export default function CapacitiesVisualizationWrapper() {
   return (
-    <AppProvider>
-      <CapacitiesTreeVisualization />
-    </AppProvider>
+    <CapacityCacheProvider>
+      <LanguageChangeHandler>
+        <CapacitiesVisualizationContent />
+      </LanguageChangeHandler>
+    </CapacityCacheProvider>
   );
 }
